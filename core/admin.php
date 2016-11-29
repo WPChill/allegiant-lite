725037c63857c61d5ef6bf2054872b5d865c0d44
Giucu <giucu91@yahoo.com>
2016-11-29 02:10:15 +0200

    #28

--- core/admin.php
+++ core/admin.php
@@ -1,38 +1,5 @@
 <?php
 
-add_action('admin_notices', 'cpotheme_admin_welcome_notice', 90);
-function cpotheme_admin_welcome_notice(){	
-	if(current_user_can('manage_options')){
-		$screen = get_current_screen();
-		$welcome_dismissed = trim(cpotheme_get_option(CPOTHEME_ID.'_wizard', 'cpotheme_settings', false));
-		
-		$display = true;
-		if(isset($_GET['action']) && $_GET['action'] == 'edit' || $screen->action == 'add' || $screen->base == 'plugins' || $screen->base == 'widgets') 
-			$display = false;
-		
-		if(current_user_can('manage_options') && $welcome_dismissed != 'dismissed' && $display){
-			$welcome_url = '<a href="'.esc_url(admin_url('themes.php?page=cpotheme-welcome')).'">'.__('quickstart guide', 'allegiant').'</a>';
-			$plugin_url = '<strong><a href="'.esc_url(admin_url('themes.php?page=cpotheme-welcome')).'">CPO Content Types</a></strong>';
-			echo '<div class="updated notice cpotheme-notice-container" style="position:relative">';
-			echo '<div class="cpotheme-notice">';
-			// echo '<a href="'.add_query_arg('ctdismiss', CPOTHEME_ID.'_wizard').'" class="cpothemes-notice-dismiss">'.__('Dismiss This Notice', 'allegiant').'</a>';
-			echo '<p>'.sprintf(esc_html__('%s is ready! Make sure you install the %s companion plugin and then check the quickstart guide to see how it all works.', 'allegiant'), esc_attr(CPOTHEME_NAME), $plugin_url, $welcome_url).'</p>';
-			echo '<p><a href="'.esc_url(admin_url('themes.php?page=cpotheme-welcome')).'" class="button" style="text-decoration: none;">'.sprintf(__('Start Using %s', 'allegiant'), esc_attr(CPOTHEME_NAME)).'</a></p>';
-			echo '</div>';
-			echo '<a href="'.add_query_arg('ctdismiss', CPOTHEME_ID.'_wizard').'" class="cpothemes-notice-dismiss notice-dismiss" style="text-decoration:none;"></a>';
-			echo '</div>';
-		}
-	}
-}
-
-//add_action('admin_menu', 'cpotheme_admin_welcome');
-function cpotheme_admin_welcome(){
-	if(current_user_can('manage_options')){
-		add_theme_page(esc_attr(CPOTHEME_NAME), esc_attr(CPOTHEME_NAME), 'read', 'cpotheme-welcome', 'cpotheme_admin_welcome_page');
-	}
-}
-
-
 function cpotheme_admin_welcome_page(){
 	echo '<div class="wrap about-wrap">';
 	$core_path = get_template_directory().'/core/';
