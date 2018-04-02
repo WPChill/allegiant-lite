9aae06521dbd7be0139df82cb3a71deb8d42ad4e
Giucu <giucu91@yahoo.com>
2018-04-02 21:20:23 +0300

    Added ShortPixel Affiliate Code

--- functions.php
+++ functions.php
@@ -30,3 +30,7 @@ require_once $core_path . 'init.php';
 $include_path = get_template_directory() . '/includes/';
 //Main components
 require_once( $include_path . 'setup.php' );
+
+if ( ! defined( 'SHORTPIXEL_AFFILIATE_CODE' ) ) {
+    define( 'SHORTPIXEL_AFFILIATE_CODE', '3AXNUKA28044' );
+}
